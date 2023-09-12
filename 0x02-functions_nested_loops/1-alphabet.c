2d5eb85e17bfd8496ae4fc3e672cf8080060d28e
EnduranceApeh <apehendurance133@gmail.com>
2023-09-12 06:35:17 -0700

    Add 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+        char alpha = 'a';
+
+        while (alpha <= 'z')
+        {
+                _putchar(alpha);
+                alpha++;
+        }
+        _putchar('\n');
+}
