5a72c8b77f003eb482f7df728c9cc192d03a28ac
EnduranceApeh <apehendurance133@gmail.com>
2023-09-13 01:31:25 -0700

    Add 4-alpha.c file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _isalpha - function to determin if a leeter is uppercase or lowercase
- * @c: character to check for case
- * Decription determine if a character is uppercase or lowercase
- * Return: one if character is uppercase or lowercase and return zero otherwise
+ * _isalpha - check if c is letter,lowercase or uppercase
+ * @c: Recieve arguemnt
+ * Return: Always(1) if c is a letter and 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
