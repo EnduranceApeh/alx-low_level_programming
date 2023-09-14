4290003a47a7e894dd05c8f39a0327a711c612dd
EnduranceApeh <apehendurance133@gmail.com>
2023-09-14 10:58:25 -0700

    work please

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* @i: store number
+* main - tests function that prints if integer is positive or negativ
+*
 * Return: 0
 */
 
