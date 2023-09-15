b0e5ad78b429b56d326adbf1ab7c38d63df4b749
EnduranceApeh <apehendurance133@gmail.com>
2023-09-15 03:02:29 -0700

    Add 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply twi integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
