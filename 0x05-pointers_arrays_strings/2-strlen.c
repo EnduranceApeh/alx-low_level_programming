ba468e7bff34d0f732cc362f83285a0faea90e64
EnduranceApeh <apehendurance133@gmail.com>
2023-09-18 11:29:03 -0700

    Add 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function that return the length of a string
+ * @s: recives argument
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return(length);
+}
