e0a4f38c85fb736154613de8b1df47ca0b982f5f
EnduranceApeh <apehendurance133@gmail.com>
2023-10-02 18:41:29 -0700

    Add 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguement it recieves
+ * @argc: arguement counter
+ * @argv: pointer to the arguement
+ * Return: 0 sucess
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+	return (0);
+}
