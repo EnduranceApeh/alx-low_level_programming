e5201b9d597cf4b319d07423508c790d0d2573ae
EnduranceApeh <apehendurance133@gmail.com>
2023-10-02 18:27:15 -0700

    Add 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguement passed to program
+ * @argc: arguement counter
+ * @argv: pointer to array of arguement
+ * Return: 0 success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+
+}
