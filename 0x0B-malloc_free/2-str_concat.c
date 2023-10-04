dd2c66e77aa91ede739246ccc8fca275a55a3a4e
EnduranceApeh <apehendurance133@gmail.com>
2023-10-04 15:39:38 -0700

    Add curly braces

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,22 +12,37 @@ char *str_concat(char *s1, char *s2)
 	int s1_len = 0;
 	int s2_len = 0;
 	char *chstr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 ==  NULL)
+	{
+		s2 = "";
+	}
 	/* concatenate strings */
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		s1_len++;
+	}
 	for (i = 0; s2[i] != '\0'; i++)
+	{
 		s2_len++;
+	}
 	/* Request for memory for s1 */
 	chstr = (char *) malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (chstr == NULL)
+	{
 		return (NULL);
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	}
 	for (i = 0; i < s1_len; i++)
+	{
 		chstr[i] = s1[i];
+	}
 	for (i = 0; i < s2_len; i++)
+	{
 		chstr[s1_len + i] = s2[i];
+	}
 	return (chstr);
 }
