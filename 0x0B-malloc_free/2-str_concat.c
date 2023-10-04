a9ce66bf18580ae3410b21425494f01ccfdd028f
EnduranceApeh <apehendurance133@gmail.com>
2023-10-04 15:17:36 -0700

    add 2-str_concate.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,15 +11,15 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, j, m, n = 0;
 	char *chstr;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	/* check for strings length*/
 	for (i = 0; s1[i] != '\0'; i++)
 
 	for (j = 0; s2[j] != '\0'; j++)
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	/* Request for memory for s1 */
 	chstr = (char *) malloc(sizeof(char) * (i + j + 1));
 	if (chstr == NULL)
