19cdb212d5aca1b4e1f0487e861bb25ce8290427
EnduranceApeh <apehendurance133@gmail.com>
2023-10-07 16:18:50 -0700

    Add 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - alloacte memory uusing malloc
+ * @b: size of memory
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+	if (ptr == NULL)
+		exit (98);
+	return (ptr);
+}
