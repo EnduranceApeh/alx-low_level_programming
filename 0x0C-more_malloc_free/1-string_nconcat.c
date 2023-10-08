ca3fce643a1cc6ae2c2ecd743581be135fa10baf
EnduranceApeh <apehendurance133@gmail.com>
2023-10-08 16:40:00 -0700

    removed one btyte

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= s2_len)
 		n = s2_len;
 	/* allocate memory */
-	ptr = malloc(sizeof(char) * s1_len + n + 1);
+	ptr = malloc(sizeof(char) * s1_len + n);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
