4a9017935ed2286feec1e188a140dcfee1929f47
EnduranceApeh <apehendurance133@gmail.com>
2023-10-10 10:28:41 -0700

    add discription to 1-init_dog.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stddef.h>
 /**
  * init_dog - function taht initiliaze a var of type struct dog
  * @d: pointer to structure
@@ -9,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
