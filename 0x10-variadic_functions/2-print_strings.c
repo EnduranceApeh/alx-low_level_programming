76c150a6bb835d687c3f49f45e0984a9f33a65c6
EnduranceApeh <apehendurance133@gmail.com>
2023-10-12 20:39:27 -0700

    Add 2-print_strings.c file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - function that print string
+ * @separator: comma and space
+ * @n: number of arguement 
+ * Return: string
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list strings;
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{	if (strings == NULL)
+		{
+			printf("(nil)");
+		}
+		printf("%s", va_arg(strings, char *));
+		if(i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	putchar('\n');
+	va_end(strings);
+}
