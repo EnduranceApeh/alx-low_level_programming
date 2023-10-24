cf1d61c9d46d7c985cc16525b3522b9457a2f0d0
EnduranceApeh <apehendurance133@gmail.com>
2023-10-24 15:33:14 -0700

    Add 5-free_listint2.c file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,8 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp;
+
 	if (*head != NULL)
 	{
-		*head = NULL;
+		while (*head != NULL)
+		{
+			tmp = *head;
+			*head = tmp->next;
+			free(tmp);
+		}
 	}
 }
