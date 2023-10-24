802ddf7072861537a38fc6c295e6a72a6ca2e0cc
EnduranceApeh <apehendurance133@gmail.com>
2023-10-24 14:09:16 -0700

    modify code

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,11 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t tmp;
-	tmp = head
+	listint_t *tmp;
+
 	while (head != NULL)
 	{
+		tmp = head;
 		head = head->next;
-		free(head);
+		free(tmp);
 	}
 }
