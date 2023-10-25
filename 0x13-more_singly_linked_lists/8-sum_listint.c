811224ee078de1caa79964ddd94524e0080c5863
EnduranceApeh <apehendurance133@gmail.com>
2023-10-24 19:17:11 -0700

    Add 8-sum_listint.c file

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_lintint - function that sum all data
+ * @head: pointer to head of list
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+	sum = 0;
+
+	if (head == NULL)
+	{
+		return (sum);
+	}
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
