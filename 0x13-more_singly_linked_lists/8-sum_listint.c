fc15e451411fd3f91fa0a7c9b14d9842ef42525d
EnduranceApeh <apehendurance133@gmail.com>
2023-10-24 19:19:09 -0700

    betty style

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 /**
- * sum_lintint - function that sum all data
+ * sum_listint - function that sum all data
  * @head: pointer to head of list
  * Return: sum
  */
 int sum_listint(listint_t *head)
 {
 	int sum;
+
 	sum = 0;
 
 	if (head == NULL)
